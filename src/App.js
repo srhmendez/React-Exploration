dd9da2b86e5b7484038dc74c2a082aef689bdff6
Sarah Mendez <srhmendez@gmail.com>
2021-04-21 22:09:11 -0600

    My First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import Tweet from './tweet';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Tweet name="Dev Ed" message="This is a random Tweet :)"/>
+      <Tweet name="John Snow" message="Feeling sad :/"/>
+      <Tweet name="Traverse Media" message="Now Hiring Office Plebs"/>
+      <Tweet name="Mosh" message="Felt cute, might delete l8r :P"/>
     </div>
   );
 }
